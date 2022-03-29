20cc554e482b4958fd30b7b87cbccae724479b37
ehsangh7 <ehsanghochagh@gmail.com>
2022-03-29 20:57:18 +0430

    add new sorts

--- 02_Sorting/02_02_Comparison_of_Sorting_Algorithms/src/codes/mysorts.py
+++ 02_Sorting/02_02_Comparison_of_Sorting_Algorithms/src/codes/mysorts.py
@@ -1,9 +1,12 @@
 def insertion_sort(A):
-    for k in range(1, len(A)):       # Insert all elements 2 to n
-        item = A[k]             # The k'th element to be inserted
-        i = k             # i will hold the position of insertion
+    for k in range(1, len(A)):
+
+        item = A[k]
+
+        i = k       
         while i > 0 and A[i-1] > item:
-            A[i] = A[i-1]                        # Shift to right
+            A[i] = A[i-1]
+
             i -= 1
         A[i] = item
     res = A
@@ -12,11 +15,14 @@ def insertion_sort(A):
 def selection_sort(A):
     n = len(A)
     for i in range(n-1):
-        index = i           # The index of min remaining item
+        index = i
+
         for j in range(i+1, n):      
-            if A[index] > A[j]:  # Finding min remaining item
+            if A[index] > A[j]:
+
                 index = j
-        A[i], A[index] = A[index], A[i]                # Swap
+        A[i], A[index] = A[index], A[i]             
+
     res = A
     return res
 
@@ -30,13 +36,61 @@ def bubble_sort(A):
             if A[j] < A[j-1]:
                 A[j],A[j-1] = A[j-1], A[j]
                 bubble_found = True
-        if not bubble_found:     # Stopping when array is sorted
+        if not bubble_found:
+
             break
     res = A
     return res
 
+def get_bucket(x):
+    return x // 10
+
+def bucket_sort(A):
+
+    max_num = max(A)
+    bucket_num = get_bucket(max_num) + 1
+    buckets = [[] for i in range(bucket_num)]
+
+    for i in A:
+        buckets[get_bucket(i)] += [i]
+
+    for i in range(bucket_num):
+        buckets[i] = selection_sort(buckets[i])
+    res = []
+    for i in range(bucket_num):
+        res += buckets[i]
+
+    return res
+
+def radix_sort(A):
+
+    max_num = max(A)
+    radix = 1
+
+    while radix <= max_num:
+        B = [[] for i in range(10)] 
+
+        for i in A:
+            B[(i//radix)%10] += [i]
+
+        A = []
+
+        for i in range(10):
+            A += B[i]
+
+        radix *= 10
+
+    return A
 
 if __name__ == '__main__':
-    A = [33750, 11161, 20704, 60193, 19536]
-    selection_sort(A)
-    bubble_sort(A)
+    A = [1523, 1, 19, 3229, 4, 16, 25, 909, 223, 1648]
+    
+    print("insertion sort",insertion_sort(A))
+
+    print("selection sort",selection_sort(A))
+
+    print("bubble sort",bubble_sort(A))
+
+    print("bucket sort",bucket_sort(A))
+    
+    print("radix sort",radix_sort(A))
