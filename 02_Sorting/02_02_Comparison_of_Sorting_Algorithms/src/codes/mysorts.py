d656bdc47bb6b019626cf9961cdddf3d75b9e836
Kazem Forghani <kaz.forghani@gmail.com>
2023-07-05 17:35:51 +0330

    Rewrote mysorts.py

--- 02_Sorting/02_02_Comparison_of_Sorting_Algorithms/src/codes/mysorts.py
+++ 02_Sorting/02_02_Comparison_of_Sorting_Algorithms/src/codes/mysorts.py
@@ -1,65 +1,70 @@
 def insertion_sort(A):
-    for k in range(1, len(A)):
-        item = A[k]
+    res = A[:]
+    for k in range(1, len(res)):
+        item = res[k]
         i = k
-        while i > 0 and A[i-1] > item:
-            A[i] = A[i-1]
+        while i > 0 and res[i-1] > item:
+            res[i] = res[i-1]
             i -= 1
-        A[i] = item
-    return A
+        res[i] = item
+    return res
 
 
 def selection_sort(A):
-    n = len(A)
+    res = A[:]
+    n = len(res)
     for i in range(n - 1):
         index = i
         for j in range(i + 1, n):
-            if A[index] > A[j]:
+            if res[index] > res[j]:
                 index = j
-        A[i], A[index] = A[index], A[i]
-    return A
+        res[i], res[index] = res[index], res[i]
+    return res
 
 
 def bubble_sort(A):
-    n = len(A)
+    res = A[:]
+    n = len(res)
     for i in range(n-1):
         bubble_found = False
         for j in range(n-1, i, -1):
-            if A[j] < A[j-1]:
-                A[j], A[j-1] = A[j-1], A[j]
+            if res[j] < res[j-1]:
+                res[j], res[j-1] = res[j-1], res[j]
                 bubble_found = True
         if not bubble_found:
             break
-    return A
+    return res
 
 
 def bucket_sort(A):
-    max_num = max(A)
+    res = A[:]
+    max_num = max(res)
     bucket_num = max_num // 10 + 1
     buckets = [[] for i in range(bucket_num)]
     
-    for i in A:
+    for i in res:
         buckets[i // 10] += [i]
 
     for i in range(bucket_num):
         buckets[i] = selection_sort(buckets[i])
 
-    A = []
+    res = []
     for i in range(bucket_num):
-        A += buckets[i]
+        res += buckets[i]
     
-    return A
+    return res
 
 
 def radix_sort(A):
-    max_num = max(A)
+    res = A[:]
+    max_num = max(res)
     radix = 1
     while radix <= max_num:
         B = [[] for i in range(10)]
-        for i in A:
+        for i in res:
             B[int((i / radix) % 10)] += [i]
-        A = []
+        res = []
         for i in range(10):
-            A += B[i]
+            res += B[i]
         radix *= 10
-    return A
+    return res
